834244e112ae8d6c26884352dce0338b066b6e4e
taknikishiksha <shobhitsinghcri@gmail.com>
2025-12-25 23:22:12 +0530

    Create bootstrap.js

---
+++ resources/js/bootstrap.js
@@ -0,0 +1,11 @@
+import _ from 'lodash';
+import axios from 'axios';
+
+window._ = _;
+
+/**
+ * axios global config
+ */
+
+window.axios = axios;
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
